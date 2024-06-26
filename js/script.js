00796cf9e367e92563c0a30208090ff861037379
Rafik <rafik.azzi@etu.univ-lyon1.fr>
2024-06-26 21:11:49 +0200

    modifications de base

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('about-link')) {
+  document.getElementById('about-link').addEventListener('click', () => {
+    document.getElementById('about-section').scrollIntoView({behavior: "smooth"})
+  })
+}
