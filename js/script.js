c9cb8cedad9ef566e266f6d71c95159bd79a90c9
Rafik <rafik.azzi@etu.univ-lyon1.fr>
2024-06-26 22:54:47 +0200

    mise en place des compétences uni

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ if (document.getElementById('about-link')) {
     document.getElementById('about-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('college-skills-link')) {
+  document.getElementById('college-skills-link').addEventListener('click', () => {
+    document.getElementById('college-skills-section').scrollIntoView({behavior: "smooth"})
+  })
+}
